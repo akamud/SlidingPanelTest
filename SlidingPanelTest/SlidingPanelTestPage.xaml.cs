9bbfb56361e2508139c9dfade1a68f96d3e785dd
Mahmoud Ali <muddibr@gmail.com>
2017-04-28 18:25:14 -0400

    Sample showing a sliding panel concept

---
+++ SlidingPanelTest/SlidingPanelTestPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using DK.SlidingPanel.Interface;
+using ReactiveUI;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+
+namespace SlidingPanelTest
+{
+    public partial class SlidingPanelTestPage : ContentPage
+    {
+        async void Handle_Tapped(object sender, System.EventArgs e)
+        {
+            var initialPosition = Main.Height;
+            var currentPosition = body.Height;
+
+            await body.TranslateTo(0, (initialPosition - currentPosition) * -1, 500, Easing.SinIn);
+        }
+
+        public SlidingPanelTestPage()
+        {
+            InitializeComponent();
+
+            body.TranslationY = Main.Height + 400;
+        }
+    }
+}
